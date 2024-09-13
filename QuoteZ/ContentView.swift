96124d9bd1fd16235dab0253f2cad978fdee2872
Ibrahim-Lokman <ibrahimlokman.bd@gmail.com>
2024-09-13 17:30:22 +0600

    Initial Commit

---
+++ QuoteZ/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  QuoteZ
+//
+//  Created by Ibrahim Lokman on 13/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ QuoteZ/QuoteZApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuoteZApp.swift
+//  QuoteZ
+//
+//  Created by Ibrahim Lokman on 13/9/24.
+//
+
+import SwiftUI
+
+@main
+struct QuoteZApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
